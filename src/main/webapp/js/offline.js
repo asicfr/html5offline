c73bd403970a74593306b803e20916453c8d3ff4
asic <asicfr@gmail.com>
2012-12-04 09:51:03 +0100

    SLA init html5 offline

---
+++ src/main/webapp/js/offline.js
@@ -0,0 +1,27 @@
+ 
+function getData(){
+	if(window.localStorage){
+		for(var i=0; i<window.localStorage.length;i++){
+			var key = window.localStorage.key(i);
+			var value = window.localStorage.getItem(key);
+                                    displayData(value);
+		}
+	}
+}
+ 
+function saveData(){
+	var texte = document.getElementById("data").value;
+	if(texte){
+		if(window.localStorage){
+		window.localStorage.setItem(key, texte);
+		}
+                        displayData(texte);
+	}
+}
+ 
+function displayData(data){
+	var puce = document.createElement("li");
+	var contenu = document.createTextNode(data);
+	puce.appendChild(contenu);
+	document.getElementById("liste").appendChild(puce);
+}
